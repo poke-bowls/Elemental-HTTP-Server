730b5d9e48b2dd1a732732c631fc26a3a395d7b4
Micah Koki <micah.koki@gmail.com>
2016-01-09 12:09:33 -0800

    Work on dynamically updating template

--- server.js
+++ server.js
@@ -16,33 +16,33 @@ function onConnect( request, response ) {
   }
 
   // if( request.method === 'GET' ) {
-    //can use if/else statements or switch cases to handle instances of each method
+  //   // can use if/else statements or switch cases to handle instances of each method
 
-    // fs.readFile( './public/' + path, function( err, data ){
-    //   if( err ){
-    //     return notFound();
-    //   }
-    //   response.writeHead( 200, {
-    //         'Server' : 'Narnia\n',
-    //         'Content-Length':  data.length + '\n',
-    //         // 'Content-Type': 'text/' + path.split('.');
-    //       });
-    //   return response.end( data );
-    // });
+  //   fs.readFile( './public/' + path, function( err, data ){
+  //     if( err ){
+  //       return notFound();
+  //     }
+  //     response.writeHead( 200, {
+  //           'Server' : 'Narnia\n',
+  //           'Content-Length':  data.length + '\n',
+  //           // 'Content-Type': 'text/' + path.split('.');
+  //         });
+  //     return response.end( data );
+  //   });
 
-    // var notFound = function () {
-    //   fs.readFile( './public/404.html' , function( err, data ) {
-    //     if(err) console.log(err);
+  //   var notFound = function () {
+  //     fs.readFile( './public/404.html' , function( err, data ) {
+  //       if(err) console.log(err);
 
-    //     response.writeHead( 404, {
-    //       'Server' : 'Narnia\n',
-    //       'Content-Length':  data.length + '\n',
-    //       // 'Content-Type':
-    //     });
-    //     response.write( data );
-    //   return response.end( data );
-    //   });
-    // };
+  //       response.writeHead( 404, {
+  //         'Server' : 'Narnia\n',
+  //         'Content-Length':  data.length + '\n',
+  //         // 'Content-Type':
+  //       });
+  //       response.write( data );
+  //     return response.end( data );
+  //     });
+  //   };
 
   // } else if( request.method === 'POST' ) {
 
@@ -73,22 +73,32 @@ function onConnect( request, response ) {
               if (err) console.log(err);
               var excludedFiles = [ '.keep', '404.html', 'css', 'index.html', 'indexTemplate.html', '.DS_Store', 'elementTemplate.html' ];
               var elementArr = files.filter( function( curr, index, array ){
-
               return excludedFiles.indexOf( curr ) === -1;
             });
+            console.log( elementArr );
+
             elements = elementArr.length;
 
             fs.readFile( './public/indexTemplate.html', function( err, data ) {
               var number = { numberElements: elements};
 
               var source = data.toString();
+              console.log( source );
               var template = Handlebars.compile( source );
               var result = template( number );
+              var newIndex = result.replace( '</ol>', '<li>\n<a href="/' + element + '.html">' + form.elementName + '</a>\n</li>\n' + '</ol>' );
 
-              fs.writeFile( 'public/index.html', result, function( err ) {
+              fs.writeFile( 'public/indexTemplate.html', newIndex, function( err, data ) {
                 if( err ) {
                   console.log( err );
                 }
+              });
+
+              fs.writeFile( 'public/index.html', newIndex, function( err ) {
+                if( err ) {
+                  console.log( err );
+                }
+
                 response.end();
               });
             });
